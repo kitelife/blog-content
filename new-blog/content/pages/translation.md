9748863e1aaa04814d8638f7cc6b87f3a87b2e4c
youngsterxyf <sas.198708@gmail.com>
2016-06-26 17:10:13 +0800

    new page

---
+++ new-blog/content/pages/translation.md
@@ -0,0 +1,18 @@
+Title: 技术翻译
+Date: 2016-06-26
+Slug: translation
+
+## 书籍
+
+- 精通Python设计模式
+
+![](http://www.ituring.com.cn/bookcover/1715.489.big.jpg)
+
+## 文章
+
+[译文列表](http://blog.xiayf.cn/tag/fan-yi.html)
+
+## 文档
+
+- [Requests中文文档](http://cn.python-requests.org/zh_CN/latest/) (注：个人翻译了首个官方中文版本的大部分内容)
+- [AWK程序设计语言](http://awk.readthedocs.io/en/latest/) (注：未翻译完成)
--- new-blog/pelicanconf.py
+++ new-blog/pelicanconf.py
@@ -47,9 +47,10 @@ DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_MENU = False
 MENUITEMS = (
     (u'归 档', '/archives.html'),
+    (u'技术分享', '/pages/tech-share.html'),
+    (u'技术翻译', '/pages/translation.html'),
     (u'工具集', '/pages/tools.html'),
     (u'链 接', '/pages/links.html'),
-    (u'技术分享', '/pages/tech-share.html'),
     (u'关于我', '/pages/aboutme.html'),
     (u'RSS', '/feeds/rss.xml')
 )
